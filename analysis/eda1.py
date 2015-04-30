ed824f7d85b1e6a8d2a52c63c7f4ea752ae33bef
ilya <ilyaaltshteyn@gmail.com>
2015-04-30 18:34:20 -0400

    did eda1.py

---
+++ analysis/eda1.py
@@ -0,0 +1,31 @@
+import pandas as pd, seaborn as sns
+from matplotlib import pyplot as plt
+cols = ['age', 'workclass', 'fnlwgt', 'education', 'education-num', 'marital_status', 'occupation', 'relationship', 'race', 'sex', 'capital_gain', 'capital_loss', 'hours_per_week', 'native_country', 'income_cat']
+file = '/Users/ilya/metis/week4/big_project/metis_project_3/analysis/census_income_data.csv'
+data = pd.read_csv(file, names = cols)
+
+data = data.drop('fnlwgt', axis = 1)
+data = data.drop('education', axis = 1)
+data = data.drop('capital_gain', axis = 1)
+data = data.drop('capital_loss', axis = 1)
+
+print data.describe()
+data = data[data.age >= 18]
+
+sns.barplot(data.income_cat)
+plt.show()
+data = data[data.income_cat != 14]
+
+sns.barplot(data.sex)
+plt.show()
+
+# Print out unique values of categorical columns to look for nonsense:
+print data.race.unique()
+print data.workclass.unique()
+print data.marital_status.unique()
+print data.occupation.unique()
+
+
+
+
+
