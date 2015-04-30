e49df5456a96d15d90377fc54f5806cefa50b396
ilya <ilyaaltshteyn@gmail.com>
2015-04-30 18:35:54 -0400

    moved stuff around on local machine

--- analysis/eda1.py
+++ analysis/eda1.py
@@ -1,7 +1,7 @@
 import pandas as pd, seaborn as sns
 from matplotlib import pyplot as plt
 cols = ['age', 'workclass', 'fnlwgt', 'education', 'education-num', 'marital_status', 'occupation', 'relationship', 'race', 'sex', 'capital_gain', 'capital_loss', 'hours_per_week', 'native_country', 'income_cat']
-file = '/Users/ilya/metis/week4/big_project/metis_project_3/analysis/census_income_data.csv'
+file = '/Users/ilya/metis/week4/metis_project_3/analysis/census_income_data.csv'
 data = pd.read_csv(file, names = cols)
 
 data = data.drop('fnlwgt', axis = 1)
@@ -25,7 +25,7 @@ print data.workclass.unique()
 print data.marital_status.unique()
 print data.occupation.unique()
 
-
+data.to_csv('/Users/ilya/metis/week4/metis_project_3/analysis/clean_data.csv')
 
 
 
