99f32b967c4ae63520a099f33e4558ab42087243
ilya <ilyaaltshteyn@gmail.com>
2015-05-07 17:41:38 -0400

    added decision tree classifier and feature standardizer

--- analysis/analysis1.py
+++ analysis/analysis1.py
@@ -6,21 +6,31 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-from sklearn.svm import SVC
+from sklearn.preprocessing import scale
 from sklearn.metrics import accuracy_score
 from sklearn.learning_curve import learning_curve
 from sklearn.cross_validation import cross_val_score
 from sklearn.cross_validation import train_test_split
 from sklearn.grid_search import GridSearchCV
+from sklearn.svm import SVC
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
 
 #                           ***PREPARE DATA***
 file ='/Users/ilya/metis/week4/metis_project_3/analysis/clean_data.csv'
 data = pd.read_csv(file, header = 0)
 
+# Standardize continuous features so they're on equal scales:
+numerical_columns = [x for x in data.columns if data[x].dtype == 'int64'][:-1]
+for column in numerical_columns:
+    data[column] = data[column].astype(float)
+    data[column] = scale(data[column])
+
 # Cut out 1/10th of data for faster cross-validation:
 data = data.ix[:3999]
 
+# Select several features to use. Can't use all of them bc many are categorical
+# and have many categories.
 del data['marital_status']
 del data['relationship']
 
@@ -31,8 +41,7 @@ x = data_dummied.ix[:,:-1]
 y = data_dummied[data_dummied.columns[-1]]
 
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = .25)
-# Select several features to use. Can't use all of them bc many are categorical
-# and have many categories.
+
 
 #                       ***Run and Cross-validate models***
 # Stategy: cross-validate lots of combinations of hyperparameters using
@@ -43,7 +52,7 @@ x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = .25)
 # Models and their hyperparams to vary:
 # SVM - C, kernel, gamma (only for rbf kernel)
 # KNN - n_neighbors, weight = ['uniform', 'distance'], leaf_size, p = [1,2]
-# Decision tree - 
+# Decision tree - criterion = ['gini', 'entropy'], max_depth = [2,4,6,8,10,12], min_samples_split = [2,10,50], min_samples_leaf = [1,5,10]
 # Random forest -
 # Naive Bayes - 
 # Logistic regression -
@@ -77,14 +86,25 @@ knn_grid.fit(x_train, y_train)
 print knn_grid.best_params_
 print knn_grid.best_score_
 
-# The best KNN has n_neighbors = 100, weights = 'distance', leaf_size = 1000, p = 1
+# The best KNN has n_neighbors = 100, weights = 'uniform', leaf_size = 100, p = 2
 
 
 
 
 
 #### Decision tree
-param_grid = [{}]
+param_grid = [{
+    'criterion' : ['gini', 'entropy'], 'max_depth' : [2,4,6,8,10,12], 
+    'min_samples_split' : [2,10,50], 'min_samples_leaf' : [1,5,10]
+}]
+
+tree_grid = GridSearchCV(DecisionTreeClassifier(), param_grid, cv = 3,
+                         scoring = 'accuracy', verbose = 10)
+tree_grid.fit(x_train, y_train)
+print tree_grid.best_params_
+print tree_grid.best_score_
+
+# The best decision tree has min_samples_split = 50, criterion = 'entropy', max_depth = 4, min_samples_leaf = 5
 
 
 
